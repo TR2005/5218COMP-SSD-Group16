5f60a32a2ca53824f6a54b7db02ffd79f8198e8e
TR2005 <117948054+TR2005@users.noreply.github.com>
2026-03-12 20:14:00 +0000

    Moving comments table into main DB, Updating the DB, adding a table for posts in Home for comments dynamic loading feature to work. bug fixing and fixing SQL injection in Comments.php

--- Comments.php
+++ Comments.php
@@ -1,11 +1,8 @@
 <?php
-
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "Comments table";
-
-$conn = mysqli_connect($host,$user,$password,$dbname);
+include "Masterpage.php";
+MasterPage();
+//Connecting to DB
+$conn= new mysqli ('localhost', 'root', '', 'ssdcoursework1');
 
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
@@ -14,8 +11,6 @@ if(!$conn){
 ?>
 
 <?php
-include "config.php";
-
 $post_id = $_GET['id']; // which post is being viewed
 ?>
 
@@ -42,7 +37,7 @@ while($row = mysqli_fetch_assoc($result)){
 
 <h3>Add Comment</h3>
 
-<form action="add_comment.php" method="POST">
+<form method="POST">
 
 <input type="hidden" name="post_id" value="<?php echo $post_id; ?>">
 
@@ -55,17 +50,17 @@ while($row = mysqli_fetch_assoc($result)){
 </form>
 
 <?php
-
-include "config.php";
-
-$post_id = $_POST['post_id'];
-$comment = $_POST['comment'];
-
-$sql = "INSERT INTO comments (post_id, comment)
-        VALUES ('$post_id','$comment')";
-
-mysqli_query($conn,$sql);
-
-header("Location: view_post.php?id=".$post_id);
-
+    if(strtolower($_SERVER['REQUEST_METHOD']) == 'post'){
+        $post_id = $_POST['post_id'];
+        $comment = $_POST['comment'];
+        if ($stmt = $conn -> prepare("INSERT INTO comments (post_id, comment) Values (?, ?)")){
+            $stmt -> bind_param("ss", $post_id, $comment);
+            $stmt -> execute();
+            if ($stmt -> insert_id == 0){
+                echo "<script type ='text/javascript'>alert('Failed to add comment');/script>";
+                exit();
+            }else{header("Location: Comments.php?id=$post_id");}
+            $stmt -> close();
+        }
+    }
 ?>
--- Home.php
+++ Home.php
@@ -3,6 +3,32 @@ include "Masterpage.php";
 MasterPage();
 $Content = <<<Content
 <title>Home</title>
+    <table class="table teable-striped table-hover">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Poster</th>
+                <th>link</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Test1</td>
+                <td>User1</td>
+                <td><a href="Comments.php?id=1" class="btn btn-sm" role="button">Link</td>
+            </tr>
+            <tr>
+                <td>Test2</td>
+                <td>User2</td>
+                <td><a href="Comments.php?id=2" class="btn btn-sm" role="button">Link</td>
+            </tr>
+            <tr>
+                <td>Test3</td>
+                <td>User3</td>
+                <td><a href="Comments.php?id=3" class="btn btn-sm" role="button">Link</td>
+            </tr>
+        </tbody>
+    </table>
 Content;
 echo $Content;
 ?>
