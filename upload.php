bee1b1e6c0bf30bbeb49523c6c97262020c90b69
OceaneParish <oceanecparker1@gmail.com>
2026-03-13 04:58:06 +0000

    Finalise code & comments

--- upload.php
+++ upload.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
+
 <?php
+// Include master page so it uses the same page layout and navigation bar 
 include "Masterpage.php";
 MasterPage();
 ?>
@@ -9,12 +11,17 @@ MasterPage();
     <title>Upload File</title>
 </head>
 <body>
-
+<!-- Heading for Upload --> 
 <h2>Upload File</h2>
 
+<!-- Allows user to upload file to server. --> 
 <form action="upload-process.php" method="post" enctype="multipart/form-data">
+
+    <!-- Prompts user selects an image to upload -->
     Select file to upload:
     <input type="file" name="fileToUpload" id="fileToUpload" required>
+
+    <!-- Submit button to send file to upload-process.php --> 
     <input type="submit" value="Upload File" name="submit">
 </form>
 
